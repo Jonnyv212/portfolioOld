b55904efe8242de48fea7e97e0160742fa025024
Jonathan Vega (TMNA) <jonathan.vega@toyota.com>
2019-07-03 12:26:09 -0400

    Updates

---
+++ precache-manifest.5b0f041856a031fc388a4b17ba899b65.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b00a537e97a92e501909045657e5fdc",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2a07f9eff7556d0f5716",
+    "url": "/Portfolio/static/css/main.5dcf5525.chunk.css"
+  },
+  {
+    "revision": "941bc2c99c4def274335",
+    "url": "/Portfolio/static/js/2.7d0a0d58.chunk.js"
+  },
+  {
+    "revision": "2a07f9eff7556d0f5716",
+    "url": "/Portfolio/static/js/main.95a6d69b.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "076f06c993986a28e4eff4c36d5f014d",
+    "url": "/Portfolio/static/media/banner.076f06c9.jpg"
+  },
+  {
+    "revision": "e3ff9ec4824c1113d7407eb0a796c6a8",
+    "url": "/Portfolio/static/media/email6.e3ff9ec4.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "7ccb7a068200d2973cf68d5e4115fb55",
+    "url": "/Portfolio/static/media/linkedin2.7ccb7a06.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b9f450ec7f2bfad6779ddfedf3e07058",
+    "url": "/Portfolio/static/media/portfolio_preview.b9f450ec.png"
+  },
+  {
+    "revision": "a90a28fc7a613cf742fe7150753d16ac",
+    "url": "/Portfolio/static/media/portfolio_preview2.a90a28fc.png"
+  }
+]);
